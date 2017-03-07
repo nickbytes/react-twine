cb42a2f660b22fb5d34e6ec6542977b5f7dd7cab
Nick Beattie <nick@nickbytes.com>
2017-03-07 09:10:01 -0500

    feat: the welcome screen was annoying me

--- src/components/StartGameButtonComponent/index.js
+++ src/components/StartGameButtonComponent/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router';
 
 const StartGameButtonComponent = () => (
-  <div style={{ maxWidth: '55em', margin: '0 auto', padding: '40px 1em' }}>
+  <div style={{ maxWidth: '55em', margin: '0 auto', padding: '20px 1em' }}>
     <Link style={{ color: '#000' }} to="/intro">Begin.</Link>
   </div>
   );
--- src/components/WelcomeMessageComponent/index.js
+++ src/components/WelcomeMessageComponent/index.js
@@ -4,8 +4,10 @@ import styles from './style';
 
 const WelcomeMessage = () => (
   <div className={css(styles.welcomeContainer)}>
-    <h1 className={css(styles.heading)}>Inanimate Messenger</h1>
-    <h2 className={css(styles.version)}>v0.1 beta</h2>
+    <h1 className={css(styles.heading)}>
+      Inanimate Messenger<span className={css(styles.version)}>v0.1 beta</span>
+    </h1>
+
     <h2 className={css(styles.subHeading)}>
       A text-based adventure game.
     </h2>
